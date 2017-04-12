1de98cc98a0a419e8a1bd4841d4e181ae2ba8617
Valtteri Karesto <valtteri.karesto@gmail.com>
2017-04-12 18:40:04 +0300

    Added example for breakpoints

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
+import styles from './constants/styles';
 import './App.css';
+
+const Container = styled.div`
+  background: hotpink;
+  @media (min-width: ${styles['$grid-breakpoints'].xl}) {
+    background: teal;
+  }
+`;
 
 class App extends Component {
   render() {
@@ -10,9 +19,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </Container>
       </div>
     );
   }
